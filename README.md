11fa1325e69516445e26e07509ebf1dc7f82e550
Andrew Collier <andrew@fathomdata.dev>
2024-01-19 09:26:09 +0000

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind/Next.js
+
+A minimal site with Tailwind and Next.js.
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">
+        Hello, Next.js with Tailwind!
+      </h1>
+    </div>
+  )
+}
